2aec4f3407a5d0da8d3944db1dec3c06302f3376
tere15 <kristiina.horneman@gmail.com>
2016-01-29 14:59:35 +0200

    Version 0.0.2

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,12 @@ from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email
 
 class LoginForm(Form): # LoginForm perii Formin eli esim <input type="text"/>, <input type="submit"/> #
-		email = StringField('Enter your email',validators=[Required(),Email()])
-		passw = PasswordField('Enter password', validators=[Required()])
-		submit = SubmitField('Login')
+	email = StringField('Enter your email',validators=[Required(),Email()])
+	passw = PasswordField('Enter password', validators=[Required()])
+	submit = SubmitField('Login')
+
+class RegisterForm(Form):
+	email = StringField('Enter your email',validators=[Required(),Email()])
+	passw = PasswordField('Enter password', validators=[Required()])
+	submit = SubmitField('Login')
+	
--- app/routers.py
+++ app/routers.py
@@ -1,13 +1,26 @@
 from app import app  #haetaan app moduulista app __init__.py tiedostosta
 #render_template gives you access to Jinja2 template engine
 #router-funktion pitää aina palauttaa jotakin : sisältö tai header-tieto
-from flask import render_template, request, make_response
-from app.forms import LoginForm
+from flask import render_template, request, make_response, flash, redirect
+from app.forms import LoginForm, RegisterForm
+from app.db_models import Users
+from app import db
 
 @app.route('/',methods=['GET','POST'])
-def index():
+def index(): #instanssi login-formista
 	login = LoginForm()
-	return render_template('template_index.html',form=login)
+	#Check if get method
+	if request.method == 'GET':
+		return render_template('template_index.html',form=login)
+	else:
+		#check if form data is valid
+		if login.validate_on_submit(): #zekkaa onko validatoreilla tarkistettu data validia
+			print(login.email.data) #pythonilla ei tarvi requestista hakea dataa, vaan suoraan formilta
+			print(login.passw.data)
+			return render_template('template_user.html',form=login) # renderöidään template_user.html
+		else:
+			flash('Give proper information to email and password fields!')
+			return render_template('template_index.html',form=login)			
 	
 	
 @app.route('/user/<name>')
@@ -20,6 +33,25 @@ def user(name):
 def userParams():
 	name = request.args.get('name') #url-attribuutin sisältö name-muuttujaan
 	return render_template('template_user.html',name=name)	
+
+@app.route('/register',methods=['GET','POST'])
+def registerUser():
+	form = RegisterForm()
+	if request.method == 'GET':
+		return render_template('template_register.html',form=form)	
+	else:
+		if form.validate_on_submit():
+			user = Users(form.email.data,form.passw.data)
+			db.session.add(user)
+			db.session.commit()			
+			flash("Name {0}".format(form.email.data)) #dynaaminen merkkijono, {0} korvataan form.email.data 
+			return redirect('/')
+		else:
+			flash('Invalid email address or no password given')
+			return render_template('template_register.html',form=form)				
+			
+
+
 
 
 	
